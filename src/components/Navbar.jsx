f21e4fa2c23d792369025f1a3e0c47b3d49b0428
ripesh <ripeshkyada201@gmail.com>
2022-08-15 18:31:47 +0530

    LOgin done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,124 @@
 import React, { useContext, useState } from 'react'
 import { Link } from "react-router-dom"
 import Refcon from '../context/Refcontext';
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 // navbar login button click btn ...............
 const Navbar = () => {
+  const [data, setdata] = useState({ fname: "", lname: "", email: "", pass: "", cpass: "" })
 
   const refcon = useContext(Refcon)
-  let { reflog,refreg } = refcon
+  let { reflog, refreg, refsignup, refclose, refclosein, refsignin } = refcon
   const showModal = () => {
     reflog.current.click()
   };
-  
-  
-  
-  
-  
+
+
+
+  const changeHandler = (e) => {
+    setdata({ ...data, [e.target.name]: e.target.value })
+  }
+
   // navbar register button click btn ...............
-  
-  
+
+
   const showModal1 = () => {
     refreg.current.click()
   };
 
 
+  const subHandler = async (e) => {
+    e.preventDefault();
+    let { fname, lname, email, pass, cpass } = data;
+    const res = await fetch("http://localhost:5000/auth/signup", {
+      credentials: 'include',
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({ fname, lname, email, pass, cpass })
+    })
+    const token = await res.json();
+    if (res.status === 201) {
+      console.log("sign up done.!");
+      setdata({ fname: "", lname: "", email: "", pass: "", cpass: "" })
+      refclose.current.click()
+      
+      localStorage.setItem("token", token)
 
+    }
+    else {
+
+      toast.warn(`${token}`, {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: true,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        style: {
+          textTransform: "capitalize"
+        }
+      })
+    }
+
+  }
+
+  // sign in
+
+  const [data2, setData2] = useState({ email: "", pass: "" })
+  const subHandler2 = async (e) => {
+    e.preventDefault();
+    let { email, pass } = data2
+    const res = await fetch("http://localhost:5000/auth/signin", {
+      credentials: "include",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({ email, pass })
+    })
+    const token = await res.json()
+    if (res.status === 200) {
+      localStorage.setItem("token", token)
+      setData2({ email: "", pass: "" })
+      refclosein.current.click()
+
+    }
+    else {
+      // refclose.current.click()
+
+      toast.warn(`${token}`, {
+        position: "top-right",
+        autoClose: 2300,
+        hideProgressBar: true,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        progressClassName: 'bar',
+        style: {
+          textTransform: "capitalize"
+        }
+
+
+      });
+    }
+  }
+
+  const changeHandler2 = (e) => {
+    setData2({ ...data2, [e.target.name]: e.target.value })
+  }
+
+  const subTrigger = () => {
+    refsignup.current.click()
+  }
+  const signinTrigger = () => {
+    refsignin.current.click()
+  }
 
   return (
     <div>
@@ -82,29 +177,26 @@ const Navbar = () => {
               <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div className="modal-body">
-              <form  >
-                {/*className='border border-dark p-5 border border-2 rounded-3 shadow-lg' */}
+              <form method='post' onSubmit={subHandler2} >
                 <h1 className='text-center fw-bold '>Sign In</h1>
                 <hr className='border border-dark border border-3  ' />
-                {/* <div className="col-md-5 d-flex justify-content-center">
-            
-                        <img src="/assets/login.jpg" alt="conatct" height='300px' width='300px' />
-                    </div> */}
+
+
                 <div className="mb-3 pt-3 ">
                   <label for="exampleInputEmail1" className="form-label " >Email address</label>
-                  <input type="email" className="form-control  border-dark" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="name@example.com" />
+                  <input type="email" className="form-control  border-dark" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="name@example.com" name='email' onChange={changeHandler2} value={data2.email}/>
                   <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div className="mb-3 pt-3 pb-3">
                   <label for="exampleInputPassword1" className="form-label">Password</label>
-                  <input type="password" className="form-control  border-dark" id="exampleInputPassword1" />
+                  <input type="password" className="form-control  border-dark" id="exampleInputPassword1" name='pass' onChange={changeHandler2} value={data2.pass} />
                 </div>
-
+                <input type="submit" className='d-none' value="" ref={refsignin} />
               </form>
             </div>
             <div className="modal-footer">
-              <button type="button" class="btn btn-outline-dark" data-bs-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-dark">Login</button>
+              <button type="button" class="btn btn-outline-dark" data-bs-dismiss="modal" ref={refclosein}>Close</button>
+              <button type="button" onClick={signinTrigger} class="btn btn-dark">Login</button>
             </div>
           </div>
         </div>
@@ -129,41 +221,43 @@ const Navbar = () => {
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-              <form>
+              <form method='post' onSubmit={subHandler}>
                 <h1 className='text-center fw-bold '>Sign Up</h1>
                 <hr className='border border-dark border border-3' />
                 <div className="row">
                   <div className="col mb-3 pt-2">
                     <label for="validationCustom01" className="form-label">First Name</label>
-                    <input type="text" className="form-control border-dark" placeholder="First name" aria-label="First name" required />
+                    <input type="text" name='fname' className="form-control border-dark" placeholder="First name" aria-label="First name" onChange={changeHandler} value={data.fname} required />
                   </div>
                   <div className="col pt-2 mb-3">
                     <label for="exampleInput" className="form-label">Last Name</label>
-                    <input type="text" className="form-control border-dark" placeholder="Last name" aria-label="Last name" required />
+                    <input type="text" name='lname' className="form-control border-dark" placeholder="Last name" aria-label="Last name" required onChange={changeHandler}  value={data.lname}/>
                   </div>
                 </div>
                 <div className="pt-1 pb-3">
-                  <label for="exampleInputEmail1" className="form-label">Email address</label>
-                  <input type="email" className="form-control  border-dark" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="name@example.com" />
+                  <label for="exampleInputEmail2" className="form-label">Email address</label>
+                  <input type="email" name='email' className="form-control  border-dark" id="exampleInputEmail2" aria-describedby="emailHelp" placeholder="name@example.com" onChange={changeHandler} value={data.email} />
                   <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div className=" pt-1 pb-3">
                   <label for="exampleInputPassword1" className="form-label">Password</label>
-                  <input type="password" className="form-control  border-dark" id="exampleInputPassword1" />
+                  <input type="password" name='pass' className="form-control  border-dark" id="exampleInputPassword1" onChange={changeHandler} value={data.pass}/>
                 </div>
                 <div className="mb-3 pt-1 pb-3">
                   <label for="exampleInputPassword1" className="form-label">Confirm Password</label>
-                  <input type="password" className="form-control  border-dark" id="exampleInputPassword1" />
+                  <input type="password" name='cpass' className="form-control  border-dark" id="exampleInputPassword1" onChange={changeHandler} value={data.cpass}/>
                 </div>
+                <input type="submit" className='d-none' value="" ref={refsignup} />
               </form>
             </div>
             <div class="modal-footer">
-              <button type="button" class="btn btn-outline-dark" data-bs-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-dark">Register</button>
+              <button type="button" class="btn btn-outline-dark" data-bs-dismiss="modal" ref={refclose}>Close</button>
+              <button type="button" onClick={subTrigger} class="btn btn-dark">Register</button>
             </div>
           </div>
         </div>
       </div>
+      <ToastContainer />
     </div>
   )
 }
--- src/context/Contextprovider.js
+++ src/context/Contextprovider.js
@@ -4,8 +4,12 @@ import Refcon from "./Refcontext";
 function Contextprovider(props) {
     let reflog=useRef(null)
     let refreg=useRef(null)
+    let refsignup=useRef(null)
+    let refclose=useRef(null)
+    let refsignin=useRef(null)
+    let refclosein=useRef(null)
   return <>
-    <Refcon.Provider value={{reflog,refreg}}>
+    <Refcon.Provider value={{reflog,refreg,refsignup,refclose,refsignin,refclosein}}>
         {props.children}
     </Refcon.Provider>
   </>;
